e00bab0cca535a0498a6afb667c7d58d5a39d97f
caborryer <corraleshyulia@gmail.com>
2019-10-26 12:19:56 -0500

    modificacion mensaje confirmado

--- client-geek/src/components/EnvioCopia.jsx
+++ client-geek/src/components/EnvioCopia.jsx
@@ -19,7 +19,10 @@ class EnvioCopia extends React.Component() {
 
         <div class="d-flex justify-content-center">
           <div class="d-flex p-2 bd-highlight ">
-            <img src="./img/cargando.gif" alt="" />
+            <img
+              src="https://pa1.narvii.com/6707/f4b9a2d144c3ae90e94cb16114b293c256a1bcef_hq.gif"
+              alt=""
+            />
           </div>
         </div>
       </div>
