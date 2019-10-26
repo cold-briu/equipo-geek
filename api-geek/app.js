a151fd06378e83a9a89b6b4119a99daf9d71f429
cold-briu <andresavena19@gmail.com>
2019-10-26 11:53:23 -0500

    sdasd:wq

--- api-geek/app.js
+++ api-geek/app.js
@@ -1,15 +1,22 @@
 const app = require('express')();
-const bodyParser = require("body-parser");
-const { port } = require("./config/config");
+const bodyParser = require('body-parser');
+const { port } = require('./config/config');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.json({ limit: '10mb' }));
 
 // Start the server
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-  console.log('Press Ctrl+C to quit.');
+	console.log(`App listening on port ${port}`);
+	console.log('Press Ctrl+C to quit.');
 });
 // [END gae_node_request_example]
+
+app.get('/', (req, res) => {
+	res
+		.status(200)
+		.send('Hello, world!')
+		.end();
+});
 
 module.exports = app;
