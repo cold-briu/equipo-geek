2fd2be9997f1a60428cb588bbe71a6e9fc968843
cold-briu <andresavena19@gmail.com>
2019-10-26 11:38:31 -0500

    sms service

--- api-geek/sms.service.js
+++ api-geek/sms.service.js
@@ -1,13 +1,11 @@
 const { tw_account, tw_token } = require('./config/config');
+
 const client = require('twilio')(tw_account, tw_token);
 
-const send_sms = () =>
-	client.messages
-		.create({
-			body: 'Hola amigos equipo geek',
-			from: '+12014196925',
-			to: '+573113391423'
-		})
-		.then(message => console.log(message.sid));
-
-send_sms();
+client.messages
+	.create({
+		body: 'Hola amigos equipo geek',
+		from: '+12014196925',
+		to: '+573113391423'
+	})
+	.then(message => console.log(message.sid));
