550fccb727b5b40da463528d19ab67545cc8a7bd
Alexander <alexandercos14@gmail.com>
2019-10-26 14:07:46 -0500

    get infinito

--- api-geek/routes/vendedor.route.js
+++ api-geek/routes/vendedor.route.js
@@ -11,5 +11,39 @@ router.route("/").post(schemaValidator(schema), async (req, res, next) => {
         next(err);
     }
 })
+    .get(async (req, res, next) => {
+        try {
+            let vendedores = await new vendedorService().getAll(req.query);
+            if (vendedores.length < 1) return res.status(400).json({ message: "vendedores no encontrados" });
+            res.status(200).json(vendedores);
+        } catch (err) {
+            next(err);
+        }
+    });
 
+router.route("/:id").get((req, res, next) => {
+    try {
+        let vendedor = await new vendedorService().getOne(req.params.id);
+        if (!vendedor) return res.status(400).json({ message: "vendedor no encontrado" });
+        res.status(200).json(vendedor)
+    } catch (err) {
+        next(err);
+    }
+})
+    .put((req, res, next) => {
+        try {
+            await new vendedorService().update(req.params.id, req.body);
+            res.status(200).json({ message: "updated successfully" });
+        } catch (err) {
+            next(err);
+        }
+    })
+    .delete((req, res, next) => {
+        try {
+            await new vendedorService().delete(req.params.id);
+            res.status(200).json({ message: "deleted successfully" });
+        } catch (err) {
+            next(err);
+        }
+    })
 module.exports = router;
